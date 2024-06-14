d0f60579f4137273ffff6347be6141c850df9059
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-06-14 15:18:49 +0100

    chore(page): adds preview deployment heading to page.tsx home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Preview Deployment</h1>
       <Button>Shorten Url</Button>
     </main>
   );
