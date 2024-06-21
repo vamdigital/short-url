d5d1ae3d78fd625547a3679816e2ef0ec71fa897
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-06-21 10:08:48 +0100

    feat(header): adds logged in user data to header ui

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -43,21 +43,29 @@ export const HeaderNav = ({ session }: { session: Session | null }) => {
             ))}
           </ul>
         </div>
-        <div className="flex min-w-52 max-mobile:mt-4 max-mobile:w-full max-mobile:border-t-[1px] max-mobile:border-t-l-gray/20 max-mobile:py-4">
+        <div className="flex w-full max-mobile:mt-4 max-mobile:w-full max-mobile:border-t-[1px] max-mobile:border-t-l-gray/20 max-mobile:py-4">
           <ul className="flex w-full items-center justify-end gap-4 font-bold text-g-violet max-mobile:flex-col max-mobile:justify-center">
             {session ? (
-              <li>
+              <li className="flex flex-col items-center justify-between">
                 <form
-                  action={logout}
-                  className="flex items-center justify-center"
+                  action={() => {
+                    logout();
+                    toggleVisibility();
+                  }}
+                  className="flex items-center justify-center gap-3"
                 >
-                  <div>
-                    <p className="text-v-d-blue">Welcome</p>
-                    <p className="text-v-d-blue">{session.user.firstName}</p>
+                  <div className="flex items-center gap-2">
+                    <div className="flex h-8 w-8 rounded-full bg-gray-200"></div>
+                    <p className="text-sm text-g-violet md:text-v-d-blue">
+                      Welcome!
+                    </p>
+                    <p className="text-sm text-cyan">
+                      {session.user.firstName}
+                    </p>
                   </div>
                   <Button
                     type="submit"
-                    className="rounded-3xl bg-cyan px-6 py-3 text-center text-sm font-bold text-white max-mobile:w-full"
+                    className="flex rounded-3xl bg-cyan px-6 py-3 text-center text-sm font-bold text-white max-mobile:w-full"
                   >
                     logout
                   </Button>
@@ -66,8 +74,12 @@ export const HeaderNav = ({ session }: { session: Session | null }) => {
             ) : (
               <>
                 <li>
-                  <Link href="/login" className="hover:text-v-d-blue">
-                    Login
+                  <Link
+                    href="/login"
+                    legacyBehavior
+                    className="hover:text-v-d-blue"
+                  >
+                    <a onClick={() => toggleVisibility()}>Login</a>
                   </Link>
                 </li>
                 <li className="items-center justify-center max-mobile:flex max-mobile:w-full">
