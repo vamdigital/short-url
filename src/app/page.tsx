38939a568e68cf3a9defaf0c3f44adef8ff86777
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-07-01 21:19:25 +0100

    feat(shorturllist): adds shorturlList component
    adds shorturlList component and wires api with UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import { auth } from '@/auth';
-import { Hero, ShortUrlFormWrapper, UrlForm } from '@/components';
+import { Hero, ShortUrlFormWrapper, UrlForm, UrlListItem } from '@/components';
 import Link from 'next/link';
+import db from '../../prisma/prisma';
 
 export default async function Home() {
   const session = await auth();
+  const user =
+    session &&
+    (await db.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+      include: {
+        urls: true,
+      },
+    }));
+
+  const urls = user?.urls || [];
 
   return (
     <section className="flex w-full flex-col items-center">
@@ -14,7 +27,7 @@ export default async function Home() {
       </div>
       <ShortUrlFormWrapper className="relative top-[-10px] lg:top-[-50px]">
         {session ? (
-          <div className="container">
+          <div className="container max-mobile:p-0">
             <UrlForm />
           </div>
         ) : (
@@ -29,6 +42,16 @@ export default async function Home() {
           </div>
         )}
       </ShortUrlFormWrapper>
+      <div className="mt-10 flex w-full flex-col max-mobile:mt-20">
+        {urls.length > 0 &&
+          urls.map((item) => (
+            <UrlListItem
+              key={item.id}
+              originalUrl={item.originalUrl}
+              shortenedUrl={item.shortenedUrl}
+            />
+          ))}
+      </div>
     </section>
   );
 }
